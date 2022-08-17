f37ae0cdae74ab421aae982fe9a402ceac6c7fc0
keita-abe1022 <keita-abe1022@gmail.com>
2022-08-17 13:32:52 +0000

    コーヒーの登録画面作成

--- app/controllers/api/coffees_controller.rb
+++ app/controllers/api/coffees_controller.rb
@@ -1,4 +1,5 @@
 class Api::CoffeesController < ApplicationController
+  protect_from_forgery :except => [:create]
   
   def index
     @coffees = Coffee.all
@@ -9,4 +10,24 @@ class Api::CoffeesController < ApplicationController
     @coffee = Coffee.find(params[:id])
     render 'show', formats: :json, handlers: 'jbuilder'
   end
+
+  def create
+    @coffee = Coffee.new(coffee_params)
+    if @coffee.save
+      head :no_content
+    else
+      render json: @coffee.errors, 
+      # バリデーションエラーを返す
+      status: :unprocessable_entity
+    end
+  end
+
+    private
+
+    def coffee_params
+      params.fetch(:coffee, {}).permit(
+        :product_name, :price, :weight, :purchashing_system,
+        :degree_of_roasting, :grind, :brew, :shop, :comment
+      )
+    end
 end
--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -17,9 +17,9 @@
       <v-btn to="">About</v-btn>
       <v-btn to="/create">コーヒーを記録</v-btn>
       <v-btn to="">MyPage</v-btn>
-      <v-btn to="/signup" v-if="!signedIn">新規登録</v-btn>
+      <!-- <v-btn to="/signup" v-if="!signedIn">新規登録</v-btn>
       <v-btn to="/signin" v-if="!signedIn">ログイン</v-btn>
-      <v-btn to="/" v-if="signedIn" @click="signOut">ログアウト</v-btn>
+      <v-btn to="/" v-if="signedIn" @click="signOut">ログアウト</v-btn> -->
     </v-toolbar-items>
       <v-btn icon>
         <!-- Material iconを使用する。mdiをつけると使用可。 -->
