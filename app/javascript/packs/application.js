7f1ab4b9ffceb57909f2f43aadcc08c2396aba0e
keita-abe1022 <keita-abe1022@gmail.com>
2022-08-24 05:23:24 +0000

    ログイン/ログアウト機能の作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '../router/router.js'
 import store from '../store/store.js'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from '../backend/axios/axios.js'
+
+Vue.use(Vuetify)
+const vuetify = new Vuetify();
+
+Vue.config.productionTip = false
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     router,
     store,
+    vuetify,
+    securedAxiosInstance,
+    plainAxiosInstance,
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
+
   console.log(app)
 })
-
--- app/javascript/router/router.js
+++ app/javascript/router/router.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import CoffeeHome from '../pages/CoffeeHome.vue'
 import CoffeeCreate from '../pages/CoffeeCreate.vue'
 import CoffeeEdit from '../pages/CoffeeEdit.vue'
+import Signup from '../pages/Signup.vue'
+import Signin from '../pages/Signin.vue'
 
 Vue.use(VueRouter)
 
@@ -12,5 +14,8 @@ export default new VueRouter( {
   { path: '/', name: 'CoffeeHome', component: CoffeeHome },
   { path: '/create', name: 'CoffeeCreate', component: CoffeeCreate },
   { path: '/edit/:id', name: 'CoffeeEdit', component: CoffeeEdit },
-  ],
+  { path: '/signup', name: 'Signup', component: Signup },
+  { path: '/signin', name: 'Signin', component: Signin }
+  ]
 });
+
