d539e724f42c916fd879efb1b356ad09ab2b39ed
keita-abe1022 <keita.abe1022@gmail.com>
2022-09-14 07:04:32 +0000

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGAX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   before_save { email.downcase! }
-  validates :user_name, presence: true
+  validates :name, presence: true
   validates :email, presence: true,
                           format: { with: VALID_EMAIL_REGAX },
                           uniqueness: { case_sensitive: false }
