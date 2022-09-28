fb62cb832790dbcfa11f9c658c92551af4487f16
keita-abe1022 <keita.abe1022@gmail.com>
2022-09-28 10:39:22 +0000

    userのテスト修正・追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # name,email,passwordが存在するユーザーの有効性
-  it "is valid with a name, email and password" do
-    user = User.new(
-      name: "Admin User",
-      email: "example@examle.com",
-      password: "foobar",
+  let(:user) { FactoryBot.build(:user) }
+  let(:another_user) { FactoryBot.build{:user} }
 
-    )
-    expect(user).to be_valid
-  end
-  # emailがnilのユーザーの無効
-  it "is invalid without an email" do
-    user = User.new(
-      email: nil,
-      password: "foobar",
-    )
-    user.valid?
-    expect(user.errors[:email]).to include ("can't be blank")
-  end
-  # 同一のemailのユーザーの無効
-  it "is invalid with a duplicate email address" do
-    User.create(
-      email: "example@example.com",
-      password: "foobar",
-    )
-    user = User.new(
-      email: "example@example.com",
-      password: "foobar",
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+  describe "validation" do
+    it "正常に新規登録できる" do
+      expect(user).to be_valid
+      user.save
+    end
+
+    context "name" do
+      it "空白でないこと" do
+        user.name = nil
+        user.valid?
+        # expect(user.errors[:name]).to include ("can't be blank")
+        expect(user.errors.added?(:name, :blank)).to be_truthy
+      end
+
+      it "20文字以内であること" do
+        user.name = 'a' * 20
+        expect(user).to be_valid
+        user.save        
+      end
+
+      it "21文字以上だと登録できないこと" do
+        user.name = 'a' * 21
+        expect(user).to be_invalid
+        expect(user.errors.of_kind?(:name, :too_long)).to be_truthy
+      end
+    end
+
+    context "email" do
+      it "is invalid without an email" do
+        user = User.create(
+          name: "user1",
+          email: nil,
+          password: "foobar",
+        )
+        user.valid?
+        expect(user.errors[:email]).to include ("can't be blank")
+      end
+
+      it "is invalid with a duplicate email address" do
+        User.create(
+          name: "user1",
+          email: "user1@user1.com",
+          password: "foobar",
+        )
+        user = User.new(
+          name: "user2",
+          email: "user1@user1.com",
+          password: "foobar2",
+        )
+        user.valid?
+        expect(user.errors[:email]).to include("has already been taken")
+      end
+    end 
   end
 end
